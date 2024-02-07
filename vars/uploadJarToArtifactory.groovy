59a1c63f66633a35c67b0fee225f2ee17707cdd4
srinivish <srini.vish@gmail.com>
2024-02-07 21:44:29 +0530

    removed dup file

--- vars/uploadJarToArtifactory.groovy
+++
@@ -1,4 +0,0 @@
-def call(string jarName, string artifactoryPublicIP) {
-    echo 'JFrog Artifactory uploadJarToArtifactory ' + ${jarName} + ' ' + ${artifactoryPublicIP}
-    sh 'curl -X PUT -u admin -p password -T' + ${jarName} + 'http://' + ${artifactoryPublicIP}+':8082/artifactory/example-repo-local/' 
-}
