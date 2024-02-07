11993b51759325fa0a6c504241278158dd18760b
srinivish <srini.vish@gmail.com>
2024-02-07 21:56:00 +0530

    fix mvn Artifactory string

--- vars/mvnArtifactory.groovy
+++ vars/mvnArtifactory.groovy
@@ -1,4 +1,4 @@
 def call(String jarName, String artifactoryPublicIP) {
-    echo "JFrog Artifactory uploadJarToArtifactory ${jarName} and IP  ${artifactoryPublicIP}"
-    sh 'curl -X PUT -u admin -p password -T' + ${jarName} + 'http://' + ${artifactoryPublicIP}+':8082/artifactory/example-repo-local/' 
+    echo "JFrog Artifactory uploadJarToArtifactory $jarName and IP  $artifactoryPublicIP"
+    sh 'curl -X PUT -u admin -p password -T' + $jarName + 'http://' + $artifactoryPublicIP +':8082/artifactory/example-repo-local/' 
 }
