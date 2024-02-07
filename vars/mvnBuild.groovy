5c217c4c46dec2554f45b2df97e90176628b7ec4
srinivish <srini.vish@gmail.com>
2024-02-07 15:23:23 +0530

    modify mvn build

--- vars/mvnBuild.groovy
+++ vars/mvnBuild.groovy
@@ -1,4 +1,5 @@
-def call(Map stageParams) {
+def call(String workDIR) {
+    dir("${workDIR}")
     echo "Building the application..."
     sh "mvn clean install -DskipTests"
     def jarFile = sh(script: 'find . -name "*.jar" -type f | head -n 1', returnStdout: true).trim()
