838240f0c1b7eba71ca86a368012e63e1d47ad40
srinivish <srini.vish@gmail.com>
2024-02-07 21:49:19 +0530

    fix mvn groovy files

--- vars/mvnArtifactory.groovy
+++ vars/mvnArtifactory.groovy
@@ -1,4 +1,4 @@
-def call(String jarName, string artifactoryPublicIP) {
+def call(String jarName, String artifactoryPublicIP) {
     echo 'JFrog Artifactory uploadJarToArtifactory ' + ${jarName} + ' ' + ${artifactoryPublicIP}
     sh 'curl -X PUT -u admin -p password -T' + ${jarName} + 'http://' + ${artifactoryPublicIP}+':8082/artifactory/example-repo-local/' 
 }
--- vars/mvnBuild.groovy
+++ vars/mvnBuild.groovy
@@ -1,6 +1,6 @@
 def call() {
     echo "Building the application..."
     sh "mvn clean install -DskipTests"
-    def jarFile = sh(script: 'find . -name "*.jar" -type f | head -n 1', returnStdout: true).trim()
-    echo "Generated JAR file: ${jarFile}"
+//    def jarFile = sh(script: 'find . -name "*.jar" -type f | head -n 1', returnStdout: true).trim()
+//    echo "Generated JAR file: ${jarFile}"
 }
