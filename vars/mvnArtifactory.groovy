85dca3de94d049d4073b7d9f35f3d49ddc00a79d
srinivish <srini.vish@gmail.com>
2024-02-07 22:07:55 +0530

    fix mvn Artifactory using def

--- vars/mvnArtifactory.groovy
+++ vars/mvnArtifactory.groovy
@@ -1,4 +1,14 @@
 def call(String jarName, String artifactoryPublicIP) {
+
+// workind code. Need to use JFrog credentials from maven    
     echo "JFrog Artifactory uploadJarToArtifactory $jarName and IP  $artifactoryPublicIP"
-    // sh 'curl -X PUT -u admin -p password -T' + $jarName + 'http://' + $artifactoryPublicIP +':8082/artifactory/example-repo-local/' 
-    sh 'curl -X PUT -u admin -p Password123456789! -T ' + jarName +' ' +  artifactoryPublicIP}
+    sh 'curl -X PUT -u admin -p Password123456789! -T ' + jarName +' ' +  artifactoryPublicIP
+
+    def user = "admin"
+    def password = "Password123456789!"
+    sh 'curl -X PUT -u' + user + ':' + password + ' -T ' + jarName +' ' +  artifactoryPublicIP
+
+
+// use JFrog credentials to upload jar to artifactory
+
+}
