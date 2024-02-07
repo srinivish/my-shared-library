7d1ff8c59128b059b07e8b3eb85c4eec1f3106e7
srinivish <srini.vish@gmail.com>
2024-02-07 21:14:36 +0530

    fixed mvnBuild script

--- vars/mvnBuild.groovy
+++ vars/mvnBuild.groovy
@@ -1,5 +1,4 @@
-def call(String workDIR) {
-    dir("${workDIR}")
+def call() {
     echo "Building the application..."
     sh "mvn clean install -DskipTests"
     def jarFile = sh(script: 'find . -name "*.jar" -type f | head -n 1', returnStdout: true).trim()
