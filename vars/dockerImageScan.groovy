76544995d6ebc3e805f1cec0c4a42eaf26d45a0e
srinivish <srini.vish@gmail.com>
2024-02-08 12:21:20 +0530

    updated trivy

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -8,7 +8,6 @@ def call(String userName, String imageName, String imageTag) {
 //sh 'docker build -t '+ userName + '/' + appName + ':' + tag +' .'
     echo "Scanning image ${userName}/${imageName}:${imageTag}"
     sh 'trivy image '+ userName + '/' + imageName + ':' + imageTag + ' > scan.txt'
-    sh 'cat scan.txt'
+//    sh 'cat scan.txt'
     echo "Trivy Scan completed"
-
 }
