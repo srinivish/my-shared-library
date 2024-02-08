2760f8f30ba5e03bc24fadae673e06d3c0e66eb2
srinivish <srini.vish@gmail.com>
2024-02-08 09:38:09 +0530

    updated push commands

---
+++ vars/dockerDeployPush.groovy
@@ -0,0 +1,22 @@
+// Deploy provided container in docker
+def call(String containerName, String dockerUser, String tag)
+// user, app, tag
+{
+// Check if container is running
+def isRunning = sh(script: "docker inspect -f '{{.State.Running}}' $containerName", returnStatus: true) == 0
+if (isRunning){
+// Container is running, then stop container and remove
+  sh "docker stop $containerName"
+  sh "docker rm -f $containerName"
+}
+// Deploy container with new build
+sh "docker run --name $containerName -d $dockerUser/$containerName:$tag"
+echo "created new container: $containerName with image: $dockerUser/$containerName:$tag"
+
+// Push new image to dockerhub
+// Push docker image to dockerhub, this step is done post deploy to ensure successfull deployment of container
+//  sh 'docker push '+ $params.DockerHubUser + '/' + $params.ImageName + ':'+ $params.ImageTag + ' . '
+echo "pushing new image: $dockerUser/$containerName:$tag""
+sh "docker push $dockerUser/$containerName:$tag"
+echo "pushed new image: $dockerUser/$containerName:$tag""
+}
